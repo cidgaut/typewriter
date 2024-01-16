9467e3b7f370343b2a21c6accd2089b21a8179a0
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 14:36:07 -0500

    added counter to each iteration, decreased overall delay, created function characterDelay to work with other strings. No test cases set up

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,16 @@
-const sentence = "hello there from lighthouse labs";
-//set delay
-let delay = 0;
-for (const char of sentence) {
+const characterDelay = function (sentence) {
+  //set delay
+  let delay = 0;
+  
+  for (const char of sentence) {
   //setTimeout to creat a counter //need to print each letter individually.
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
-}
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, delay) // <= 1s delay //changed to no delay with increases each run
+
+  //after each iteration  - need to print each letter individually. half second as full second is too long
+  delay += 500;
+  }
+};
+
+characterDelay("Let's see if I can print out this string slowly.");
