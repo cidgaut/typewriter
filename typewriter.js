2b5ee7089ae30e5748e59768a15573a21e102c33
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 14:25:22 -0500

    setting delay function to print characters. incomplete

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,9 @@
-
+const sentence = "hello there from lighthouse labs";
+//set delay
+let delay = 0;
+for (const char of sentence) {
+  //setTimeout to creat a counter //need to print each letter individually.
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+}
